bf506e53c2870ce20a11b5df4d0438b029eecb2f
Khai Nguyen <khainguyen9209@gmail.com>
2023-11-05 00:50:16 +0700

    add cors

--- main.go
+++ main.go
@@ -33,12 +33,30 @@ func makeStaticFolders() {
     }
 }
 
+func CORSMiddleware() gin.HandlerFunc {
+    return func(c *gin.Context) {
+        c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+        c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+        c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+        c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, PATCH")
+
+        if c.Request.Method == "OPTIONS" {
+            c.AbortWithStatus(204)
+            return
+        }
+
+        c.Next()
+    }
+}
+
 func main() {
     rand.Seed(time.Now().UnixNano())
     makeStaticFolders()
     port := os.Getenv("PORT")
 
     router := gin.Default()
+
+    router.Use(CORSMiddleware())
 
     router.POST("/photos", handler.UploadPhotoHandler)
     router.POST("/videos", handler.UploadVideoHandler)
